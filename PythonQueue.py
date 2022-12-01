cd1cbb219cd15a531494d104f538573cdab2201c
lolavweis <114018347+lolavweis@users.noreply.github.com>
2022-12-01 17:55:52 -0500

    Python Queue

---
+++ PythonQueue.py
@@ -0,0 +1,12 @@
+names=[]
+for x in range(0,10):
+    appendedName=input("Enter name: ")
+    names.append(appendedName)
+print (names)
+for x in range(len(names)):
+    print(names.pop(0))
+print (names)
+        
+    
+        
+    
